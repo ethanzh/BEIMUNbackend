76b7920286611f0462019c2758e038af208bf7cb
Ethan Houston <ethan.houston@gmail.com>
2017-06-14 21:14:24 +0800

    TestApp

--- BEIMUNbackend/settings.py
+++ BEIMUNbackend/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'BEIMUNbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
+}
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_database',
@@ -94,7 +94,7 @@ DATABASES = {
         'HOST': 'beimundatabase.caatsoqa576b.us-west-2.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
